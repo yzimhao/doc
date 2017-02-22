46cf6bd8d328cd833377dcb8a28966495ec720ae
yzimhao <243136536@qq.com>
2017-02-22 19:07:29 +0800

    concat pdf

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,34 @@
 var gulp = require('gulp'),
+    concat = require('gulp-concat'),
     markdownpdf = require("markdown-pdf");
 
 var output = gulp.env.output || '';
+
+
+
 
 gulp.task('md2pdf', function() {
     if(!output){
         console.log('pls input output.');
         return;
     }
-    var MD_doc = ['README.md', '安全规范.md', '书写规则.md', '编码规范.md', '注释.md'],
-        PDF_doc = MD_doc.map(function(d){
-            return output + d.replace('md', 'pdf')
-        });
+    var MD_doc = [
+            'README.md',
+            '安全规范.md',
+            '书写规则.md',
+            '编码规范.md'
+            // '注释.md'
+        ].map(function(d){
+            return './php/'+ d;
+        }),
+        PDF_doc = output + "PHP开发编码规范.pdf";
 
 
-    markdownpdf().from(MD_doc.map(function(d){
-        return './php/'+ d;
-    })).to(PDF_doc, function(){
+    gulp.src(MD_doc)
+        .pipe(concat('php-tmp-doc.md'))
+        .pipe(gulp.dest('.tmp/'));
+
+    markdownpdf().from('.tmp/php-tmp-doc.md').to(PDF_doc, function(){
         console.log("done");
     });
 });
